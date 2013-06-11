c59d4391d42900dd5dbb0843bbe5a162ed255c74
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-06-11 15:31:58 +0200

    Refactored SPARQL queries

--- knuttel2.py
+++ knuttel2.py
@@ -7,18 +7,11 @@ import math
 sparql = SPARQLWrapper("http://94.23.12.201:3030/stcn/sparql")
 sparql.setQuery("""
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX stcn: <http://stcn.data2semantics.org/vocab/>
 
 SELECT DISTINCT ?title ?author ?publisher ?year ?s
 FROM <http://knuttel.data2semantics.org>
 WHERE {
 ?s dc:title ?title .
-OPTIONAL {
-?s foaf:name ?author ;
-   dc:publisher ?publisher ;
-   stcn:prohibitionYear ?year .
-}
 }
 """)
 
@@ -32,19 +25,11 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX vocab: <http://stcn.data2semantics.org/vocab/resource/>
 
-SELECT DISTINCT ?title ?author ?publisher ?year ?p
+SELECT DISTINCT ?title ?p
 FROM <http://stcn.data2semantics.org> 
 WHERE {
 ?p rdf:type vocab:Publicatie ;
             rdfs:label ?title .
-OPTIONAL {
-?publication vocab:first_author ?a ;
-             vocab:drukker ?d ;
-             vocab:jaar ?j .
-?a rdfs:label ?author .
-?d rdfs:label ?publisher .
-?j rdfs:label ?year .
-}
 }
 """)
 
@@ -53,7 +38,7 @@ print 'Launching SPARQL query...'
 resultsSTCN = sparql.query().convert()
 print 'Done.'
 
-# print 'Computing similarities, {} x {} pairs...'.format(resultsKnuttel, resultsSTCN)
+print "Computing similarities"
 for y in resultsKnuttel["results"]["bindings"]:
     max_r = 0
     knuttel_title = y["title"]["value"]
