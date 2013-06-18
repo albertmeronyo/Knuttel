678e0b87f4296ae3afb3201311e07919f73bf815
albertmeronyo <albert.meronyo@gmail.com>
2013-06-18 12:56:12 +0200

    Refactored classes

---
+++ LMArgParser.py
@@ -0,0 +1,43 @@
+import argparse
+
+class LMArgParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='A lazy SPARQL dataset linker',
+                                              epilog='Happy linking!')
+        self.parser.add_argument('-t', 
+                                 '--threshold', 
+                                 nargs=1, 
+                                 dest='t',
+                                 type=self.restricted_float,
+                                 default=[0.9], 
+                                 help='Levenshtein ratio threshold (0.9 if not specified)')
+        self.parser.add_argument('-e',
+                                 '--endpoint',
+                                 nargs=1,
+                                 dest='e',
+                                 required=True,
+                                 help='URL address of the SPARQL endpoint to query')
+        self.parser.add_argument('-g1',
+                                 '--named-graph1',
+                                 nargs=1,
+                                 dest='g1',
+                                 required=True,
+                                 help='URI of the first named graph to link')
+        self.parser.add_argument('-g2',
+                                 '--named-graph2',
+                                 nargs=1,
+                                 dest='g2',
+                                 required=True,
+                                 help='URI of the second named graph to link')
+        self.parser.add_argument('-v',
+                                 '--verbose',
+                                 action='store_const',
+                                 const=1,
+                                 dest='v',
+                                 help='Be verbose')
+        self.args = self.parser.parse_args()
+    def restricted_float(self,x):
+        x = float(x)
+        if x < 0.0 or x > 1.0:
+            raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+        return x
--- LazyMapper.py
+++ LazyMapper.py
@@ -2,78 +2,61 @@
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 from Levenshtein import ratio
+import logging
 import math
-import argparse
+from LMArgParser import LMArgParser
 
-def restricted_float(x):
-    x = float(x)
-    if x < 0.0 or x > 1.0:
-        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
-    return x
+class LazyMapper:
+    def __init__(self):
+        self.ap = LMArgParser()
+        self.log = logging.getLogger("LazyMapper")
+        if self.ap.args.v == 1:
+            logging.basicConfig(level=logging.INFO)
+        self.doSPARQL()
+        self.doLink()
+    def doSPARQL(self):
+        sparql = SPARQLWrapper(self.ap.args.e[0])
+        sparql.setReturnFormat(JSON)
+        sparql.setQuery("""
+                        SELECT DISTINCT ?title ?s
+                        FROM <""" + self.ap.args.g1[0] + """>
+                        WHERE {
+                        ?s <http://purl.org/dc/elements/1.1/title> ?title .
+                        }
+                        """)
+        self.log.info('Launching first SPARQL query...')
+        self.resultsKnuttel = sparql.query().convert()
+        self.log.info('First query done.')
+        sparql.setQuery("""
+                        SELECT DISTINCT ?title ?p
+                        FROM <""" + self.ap.args.g2[0] + """> 
+                        WHERE {
+                        ?p <http://www.w3.org/2000/01/rdf-schema#label> ?title .
+                        }
+                        """)
+        self.log.info('Launching second SPARQL query...')
+        self.resultsSTCN = sparql.query().convert()
+        self.log.info('Second query done.')
+    def doLink(self):
+        self.log.info("Computing similarities...")
+        print "@prefix owl: <http://www.w3.org/2002/07/owl#> ."
+        for y in resultsKnuttel["results"]["bindings"]:
+            max_r = 0
+            knuttel_title = y["title"]["value"]
+            knuttel_uri = y["s"]["value"]
+            close_title = ""
+            close_uri = ""
+            for x in resultsSTCN["results"]["bindings"]:
+                stcn_title = x["title"]["value"]
+                stcn_uri = x["p"]["value"]
+                r_title = ratio(knuttel_title,
+                                stcn_title)
+                if r_title > max_r:
+                    max_r = r_title
+                    close_title = stcn_title
+                    close_uri = stcn_uri
+            if max_r > self.ap.args.t[0]:
+                print "<{}> owl:sameAs <{}> .".format(knuttel_uri, close_uri)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', 
-                    '--threshold', 
-                    nargs=1, 
-                    dest='t',
-                    type=restricted_float,
-                    default=[0.9], 
-                    help='Levenshtein ratio threshold (0.9 if not specified)')
-parser.add_argument('-e',
-                    '--endpoint',
-                    nargs=1,
-                    dest='e',
-                    required=True,
-                    help='URL address of the SPARQL endpoint to query')
-args = parser.parse_args()
-
-print "@prefix owl: <http://www.w3.org/2002/07/owl#> ."
-
-sparql = SPARQLWrapper(args.e[0])
-
-sparql.setQuery("""
-SELECT DISTINCT ?title ?s
-FROM <http://knuttel.data2semantics.org>
-WHERE {
-?s <http://purl.org/dc/elements/1.1/title> ?title .
-}
-""")
-
-sparql.setReturnFormat(JSON)
-# print 'Launching SPARQL query...'
-resultsKnuttel = sparql.query().convert()
-# print 'Done.'
-
-sparql.setQuery("""
-SELECT DISTINCT ?title ?p
-FROM <http://stcn.data2semantics.org> 
-WHERE {
-?p <http://www.w3.org/2000/01/rdf-schema#label> ?title .
-}
-""")
-
-sparql.setReturnFormat(JSON)
-# print 'Launching SPARQL query...'
-resultsSTCN = sparql.query().convert()
-# print 'Done.'
-
-# print "Computing similarities"
-for y in resultsKnuttel["results"]["bindings"]:
-    max_r = 0
-    knuttel_title = y["title"]["value"]
-    knuttel_uri = y["s"]["value"]
-    close_title = ""
-    close_uri = ""
-    for x in resultsSTCN["results"]["bindings"]:
-        stcn_title = x["title"]["value"]
-        stcn_uri = x["p"]["value"]
-        r_title = ratio(knuttel_title,
-                        stcn_title)
-        # print knuttel_title, stcn_title, r_title
-        if r_title > max_r:
-            max_r = r_title
-            close_title = stcn_title
-            close_uri = stcn_uri
-    # print "Best match of", knuttel_title, "is", close_title
-    if max_r > args.t[0]:
-        print "<{}> owl:sameAs <{}> .".format(knuttel_uri, close_uri)
+if __name__ == '__main__':
+    lm = LazyMapper()
